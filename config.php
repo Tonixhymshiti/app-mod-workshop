20c509a9ed527df15260843e5761d5ceafc403cf
Meriton <meritonii1998@gmail.com>
2025-02-20 15:20:37 +0000

    Improve env accessibility

--- config.php
+++ config.php
@@ -1,9 +1,14 @@
 <?php
 // Configurazione del database
-$db_host = 'localhost';
-$db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
+// $db_host = '34.123.201.24';
+// $db_name = 'image_catalog';
+// $db_user = 'appmod-phpapp-user';
+// $db_pass = 'testpwd';
+
+$db_host = getenv('DB_HOST') ?: 'localhost';
+$db_name = getenv('DB_NAME') ?: 'image_catalog';
+$db_user = getenv('DB_USER') ?: 'appmod-phpapp-user';
+$db_pass = getenv('DB_PASS') ?: 'wrong_password';
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
